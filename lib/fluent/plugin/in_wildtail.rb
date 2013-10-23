6e49eb4a8e48cd4735386a976cb15c95c6afcead
Sho Hashimoto <hashimoto@shokai.org>
2013-10-23 12:06:11 +0900

    implement plugin

--- lib/fluent/plugin/in_wildtail.rb
+++ lib/fluent/plugin/in_wildtail.rb
@@ -1,2 +1,12 @@
-puts "wild tail loaded!!!!"
-require "fluent/plugin/wildtail"
+module Fluent
+  class WildTailInput < TailInput
+
+    Plugin.register_input('wildtail', self)
+
+    def configure(conf)
+      conf["path"] = Dir.glob(conf["path"]).join(',')
+      super
+    end
+
+  end
+end
