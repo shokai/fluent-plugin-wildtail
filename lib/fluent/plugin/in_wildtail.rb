149e99409cc9a13c88451c33f53bdf9e1a8282d8
Sho Hashimoto <hashimoto@shokai.org>
2013-10-23 13:00:02 +0900

    use Cool.io::StatWatcher

--- lib/fluent/plugin/in_wildtail.rb
+++ lib/fluent/plugin/in_wildtail.rb
@@ -4,8 +4,38 @@ module Fluent
     Plugin.register_input('wildtail', self)
 
     def configure(conf)
-      conf["path"] = Dir.glob(conf["path"]).join(',')
+      @_raw_path = conf["path"]
+      conf["path"] = Dir.glob(@_raw_path).join(',')
       super
+    end
+
+    def start
+      res = super
+      paths = Dir.glob(@_raw_path).map{|i|
+        File.dirname(i)
+      }.uniq
+      paths.each do |fpath|
+        puts fpath
+        watcher = Fluent::WildTailInput::Watcher.new File.dirname(fpath)
+        @loop.attach watcher
+      end
+      res
+    end
+
+    class Watcher < Cool.io::StatWatcher
+
+      def initialize(path)
+        @path = path
+        super path, 0.1
+      end
+
+      def on_change(previous, current)
+        p path
+        p previous
+        p current
+        puts "-"*5
+      end
+
     end
 
   end
